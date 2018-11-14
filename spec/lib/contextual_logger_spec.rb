71d67438c3647572826daa493a8c5c486a4c7095
James Ebentier <jebentier@invoca.com>
2018-11-14 10:28:25 -0800

    add test coverage for with_context

--- spec/lib/contextual_logger_spec.rb
+++ spec/lib/contextual_logger_spec.rb
@@ -14,8 +14,28 @@ describe 'ContextualLogging' do
     expect(@logger).to respond_to(:with_context)
   end
 
-  describe 'debug' do
-    it 'should allow context be passed into info' do
+  it 'should respond to debug' do
+    expect(@logger).to respond_to(:debug)
+  end
+
+  it 'should respond to info' do
+    expect(@logger).to respond_to(:info)
+  end
+
+  it 'should respond to warn' do
+    expect(@logger).to respond_to(:warn)
+  end
+
+  it 'should respond to error' do
+    expect(@logger).to respond_to(:error)
+  end
+
+  it 'should respond to fatal' do
+    expect(@logger).to respond_to(:fatal)
+  end
+
+  describe 'inline context' do
+    it 'should print out context passed into debug' do
       expected_log_line = {
         service: 'test_service',
         message: 'this is a test',
@@ -27,10 +47,8 @@ describe 'ContextualLogging' do
       expect_any_instance_of(Logger::LogDevice).to receive(:write).with("#{expected_log_line}\n")
       expect(@logger.debug('this is a test', service: 'test_service')).to eq(true)
     end
-  end
 
-  describe 'info' do
-    it 'should allow context be passed into info' do
+    it 'should print out context passed into info' do
       expected_log_line = {
         service: 'test_service',
         message: 'this is a test',
@@ -42,10 +60,8 @@ describe 'ContextualLogging' do
       expect_any_instance_of(Logger::LogDevice).to receive(:write).with("#{expected_log_line}\n")
       expect(@logger.info('this is a test', service: 'test_service')).to eq(true)
     end
-  end
 
-  describe 'warn' do
-    it 'should allow context be passed into info' do
+    it 'should print out context passed into warn' do
       expected_log_line = {
         service: 'test_service',
         message: 'this is a test',
@@ -57,10 +73,8 @@ describe 'ContextualLogging' do
       expect_any_instance_of(Logger::LogDevice).to receive(:write).with("#{expected_log_line}\n")
       expect(@logger.warn('this is a test', service: 'test_service')).to eq(true)
     end
-  end
 
-  describe 'error' do
-    it 'should allow context be passed into info' do
+    it 'should print out context passed into error' do
       expected_log_line = {
         service: 'test_service',
         message: 'this is a test',
@@ -72,10 +86,8 @@ describe 'ContextualLogging' do
       expect_any_instance_of(Logger::LogDevice).to receive(:write).with("#{expected_log_line}\n")
       expect(@logger.error('this is a test', service: 'test_service')).to eq(true)
     end
-  end
 
-  describe 'fatal' do
-    it 'should allow context be passed into info' do
+    it 'should print out context passed into fatal' do
       expected_log_line = {
         service: 'test_service',
         message: 'this is a test',
@@ -86,6 +98,57 @@ describe 'ContextualLogging' do
 
       expect_any_instance_of(Logger::LogDevice).to receive(:write).with("#{expected_log_line}\n")
       expect(@logger.fatal('this is a test', service: 'test_service')).to eq(true)
+    end
+  end
+
+  describe 'with_context block' do
+    it 'should print out the wrapper context when logging' do
+      expected_log_line = {
+        service: 'test_service',
+        message: 'this is a test',
+        severity: 'INFO',
+        timestamp: Time.now,
+        progname: nil
+      }.to_json
+
+      expect_any_instance_of(Logger::LogDevice).to receive(:write).with("#{expected_log_line}\n")
+
+      @logger.with_context(service: 'test_service') do
+        expect(@logger.info('this is a test')).to eq(true)
+      end
+    end
+
+    it 'should merge inline context into wrapper context when logging' do
+      expected_log_line = {
+        service: 'test_service',
+        file: 'this_file.json',
+        message: 'this is a test',
+        severity: 'INFO',
+        timestamp: Time.now,
+        progname: nil
+      }.to_json
+
+      expect_any_instance_of(Logger::LogDevice).to receive(:write).with("#{expected_log_line}\n")
+
+      @logger.with_context(service: 'test_service') do
+        expect(@logger.info('this is a test', file: 'this_file.json')).to eq(true)
+      end
+    end
+
+    it 'should take inline context over wrapper context when logging' do
+      expected_log_line = {
+        service: 'test_service_2',
+        message: 'this is a test',
+        severity: 'INFO',
+        timestamp: Time.now,
+        progname: nil
+      }.to_json
+
+      expect_any_instance_of(Logger::LogDevice).to receive(:write).with("#{expected_log_line}\n")
+
+      @logger.with_context(service: 'test_service') do
+        expect(@logger.info('this is a test', service: 'test_service_2')).to eq(true)
+      end
     end
   end
 end
